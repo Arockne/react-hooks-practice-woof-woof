ea7609fc699e01e84a28ace11b034657fc955fee
Daniel Calvin <arockne2003@yahoo.com>
2021-09-13 14:59:04 -0700

    commit tree 9761f2b736beef52b67946e047b67d7100780444

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -1,15 +1,24 @@
 import React from 'react'
 
-function DogList({ pups, onMoreInfo }) {
-
+function DogList({ pups, onMoreInfo, onOnlyGoodDog, onlyGoodDog }) {
   return (
     <>
       <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={() => onOnlyGoodDog(onlyGoodDog => !onlyGoodDog)}>Filter good dogs: {onlyGoodDog ? 'ON' : 'OFF'}</button>
       </div>
       <div id="dog-bar">
         {
-          pups.map(pup => <span key={pup.id} onClick={() => onMoreInfo(pup)}>{pup.name}</span>)
+          pups.map(pup => (
+              <span 
+                key={pup.id} 
+                onClick={() => { 
+                  onMoreInfo(pup) 
+                } }
+              >
+              {pup.name}
+              </span>
+            )
+          )
         }
       </div>
     </>
